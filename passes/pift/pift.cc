be37409515513abcc3f92435b62253e1623dfee0
Phantom1003 <phantom@zju.edu.cn>
2023-11-04 20:07:57 +0800

    reorganize mem

--- passes/pift/pift.cc
+++ passes/pift/pift.cc
@@ -1,63 +1,9 @@
 #include "kernel/yosys.h"
+#include "kernel/rtlil.h"
+#include "kernel/utils.h"
+#include "kernel/log.h"
 
 USING_YOSYS_NAMESPACE
-
-extern RTLIL::Cell *addTaintCell_1I1O(
-	RTLIL::Module *module, const std::string &type,
-	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_y, 
-	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_y_t, 
-	bool is_signed, const std::string &src);
-
-extern RTLIL::Cell *addTaintCell_2I1O(
-	RTLIL::Module *module, const std::string &type,
-	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_b, const RTLIL::SigSpec &sig_y,
-	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_b_t, const RTLIL::SigSpec &sig_y_t, 
-	bool is_signed, const std::string &src);
-
-extern RTLIL::Cell *addTaintCell_mux(
-	RTLIL::Module *module, const std::string &type,
-	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_b, const RTLIL::SigSpec &sig_s, const RTLIL::SigSpec &sig_y, 
-	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_b_t, const RTLIL::SigSpec &sig_s_t, const RTLIL::SigSpec &sig_y_t, 
-	const std::string &src);
-
-extern RTLIL::Cell* addTaintCell_sdff(
-	RTLIL::Module *module, 
-	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_srst, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
-	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
-	RTLIL::Const srst_value, bool clk_polarity, bool srst_polarity, const std::string &src);
-
-extern RTLIL::Cell* addTaintCell_sdffe(
-	RTLIL::Module *module, 
-	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_srst, const RTLIL::SigSpec &sig_en, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
-	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
-	RTLIL::Const srst_value, bool clk_polarity, bool en_polarity, bool srst_polarity, const std::string &src);
-
-extern RTLIL::Cell* addTaintCell_dff(
-	RTLIL::Module *module, 
-	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
-	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
-	bool clk_polarity, const std::string &src);
-
-extern RTLIL::Cell* addTaintCell_dffe(
-	RTLIL::Module *module, 
-	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_en, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
-	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
-	bool clk_polarity, bool en_polarity, const std::string &src);
-
-extern RTLIL::Cell* addTaintCell_mem(
-	RTLIL::Module *module, RTLIL::Cell *origin, 
-	const RTLIL::SigSpec &sig_rd_clk, const RTLIL::SigSpec &sig_rd_en, const RTLIL::SigSpec &sig_rd_arst, const RTLIL::SigSpec &sig_rd_srst, const RTLIL::SigSpec &sig_rd_addr, const RTLIL::SigSpec &sig_rd_data,
-	const RTLIL::SigSpec &sig_wr_clk, const RTLIL::SigSpec &sig_wr_en, const RTLIL::SigSpec &sig_wr_addr, const RTLIL::SigSpec &sig_wr_data,
-	const RTLIL::SigSpec &sig_rd_en_t, const RTLIL::SigSpec &sig_rd_addr_t, const RTLIL::SigSpec &sig_rd_data_t,
-	const RTLIL::SigSpec &sig_wr_en_t, const RTLIL::SigSpec &sig_wr_addr_t, const RTLIL::SigSpec &sig_wr_data_t);
-
-extern void addTaintCell_adff(
-	RTLIL::Module *module, RTLIL::Cell *origin, 
-	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t);
-
-extern void addTaintCell_sdffce(
-	RTLIL::Module *module, RTLIL::Cell *origin,
-	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t);
 
 PRIVATE_NAMESPACE_BEGIN
 
@@ -115,6 +61,65 @@ struct PIFTWorker {
 	bool verbose = false;
 	unsigned long taint_num = 1;
 	std::vector<string> ignore_ports;
+
+	RTLIL::Cell *addTaintCell_1I1O(
+	RTLIL::Module *module, const std::string &type,
+	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_y, 
+	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_y_t, 
+	bool is_signed, const std::string &src);
+
+	RTLIL::Cell *addTaintCell_2I1O(
+	RTLIL::Module *module, const std::string &type,
+	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_b, const RTLIL::SigSpec &sig_y,
+	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_b_t, const RTLIL::SigSpec &sig_y_t, 
+	bool is_signed, const std::string &src);
+
+	RTLIL::Cell *addTaintCell_mux(
+	RTLIL::Module *module, const std::string &type,
+	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_b, const RTLIL::SigSpec &sig_s, const RTLIL::SigSpec &sig_y, 
+	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_b_t, const RTLIL::SigSpec &sig_s_t, const RTLIL::SigSpec &sig_y_t, 
+	const std::string &src);
+
+	RTLIL::Cell* addTaintCell_sdff(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_srst, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	RTLIL::Const srst_value, bool clk_polarity, bool srst_polarity, const std::string &src);
+
+	RTLIL::Cell* addTaintCell_sdffe(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_srst, const RTLIL::SigSpec &sig_en, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	RTLIL::Const srst_value, bool clk_polarity, bool en_polarity, bool srst_polarity, const std::string &src);
+
+	RTLIL::Cell* addTaintCell_dff(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	bool clk_polarity, const std::string &src);
+
+	RTLIL::Cell* addTaintCell_dffe(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_en, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	bool clk_polarity, bool en_polarity, const std::string &src);
+
+	void addTaintCell_adff(
+	RTLIL::Module *module, RTLIL::Cell *origin, 
+	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t);
+
+	void addTaintCell_sdffce(
+	RTLIL::Module *module, RTLIL::Cell *origin,
+	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t);
+
+	void addTaintCell_mem(RTLIL::Module *module, RTLIL::Cell *origin);
+
+	RTLIL::SigSpec get_port(RTLIL::Cell *cell, RTLIL::IdString &port_name) {
+		if (cell->hasPort(port_name))
+			return cell->getPort(port_name);
+		else
+			return RTLIL::SigSpec(RTLIL::State::Sx, 1);
+	}
 
 	std::vector<RTLIL::SigSpec> get_taint_signals(RTLIL::Module *module, const RTLIL::SigSpec &sig) {
 		std::vector<RTLIL::SigSpec> sig_t(taint_num);
@@ -381,46 +386,8 @@ struct PIFTWorker {
 					addTaintCell_adff(module, c, port_taint[D][taint_id], port_taint[Q][taint_id]);
 				}
 			}
-			else if (c->type.in(ID($mem_v2))) {
-				enum PORT_NAME {
-					RD_CLK, RD_EN, RD_ARST, RD_SRST, RD_ADDR, RD_DATA,
-					WR_CLK, WR_EN, WR_ADDR, WR_DATA,
-					PORT_NUM
-				};
-			    RTLIL::SigSpec port[PORT_NUM] = {
-					c->getPort(ID::RD_CLK),
-					c->getPort(ID::RD_EN),
-					c->getPort(ID::RD_ARST),
-					c->getPort(ID::RD_SRST),
-					c->getPort(ID::RD_ADDR),
-					c->getPort(ID::RD_DATA),
-					c->getPort(ID::WR_CLK),
-					c->getPort(ID::WR_EN),
-					c->getPort(ID::WR_ADDR),
-					c->getPort(ID::WR_DATA)
-				};
-				std::vector<RTLIL::SigSpec> port_taint[PORT_NUM] = {
-				  get_taint_signals(module, port[RD_CLK]),
-				  get_taint_signals(module, port[RD_EN]),
-				  get_taint_signals(module, port[RD_ARST]),
-				  get_taint_signals(module, port[RD_SRST]),
-				  get_taint_signals(module, port[RD_ADDR]),
-				  get_taint_signals(module, port[RD_DATA]),
-				  get_taint_signals(module, port[WR_CLK]),
-				  get_taint_signals(module, port[WR_EN]),
-				  get_taint_signals(module, port[WR_ADDR]),
-				  get_taint_signals(module, port[WR_DATA])
-				};
-
-				for (unsigned long taint_id = 0; taint_id < taint_num; taint_id++) {
-					addTaintCell_mem(
-						module, c,
-						port[RD_CLK], port[RD_EN], port[RD_ARST], port[RD_SRST], port[RD_ADDR], port[RD_DATA],
-						port[WR_CLK], port[WR_EN], port[WR_ADDR], port[WR_DATA],
-						port_taint[RD_EN][taint_id], port_taint[RD_ADDR][taint_id], port_taint[RD_DATA][taint_id],
-						port_taint[WR_EN][taint_id], port_taint[WR_ADDR][taint_id], port_taint[WR_DATA][taint_id]);
-				}
-			}
+			else if (c->type.in(ID($mem_v2)))
+				addTaintCell_mem(module, c);
 
 			// module
 			else if (module->design->module(c->type) != nullptr) {
@@ -478,6 +445,241 @@ struct PIFTWorker {
 		instrument_wire(module);
 	}
 };
+
+RTLIL::Cell * PIFTWorker::addTaintCell_1I1O(
+	RTLIL::Module *module, const std::string &type,
+	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_y, 
+	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_y_t, 
+	bool is_signed, const std::string &src) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_1I1O));
+	cell->setParam(ID(TYPE), type);
+	cell->setParam(ID::A_SIGNED, is_signed);
+	cell->setParam(ID::A_WIDTH, sig_a.size());
+	cell->setParam(ID::Y_WIDTH, sig_y.size());
+	cell->setPort(ID::A, sig_a);
+	cell->setPort(ID::Y, sig_y);
+	cell->setPort(ID(A_t), sig_a_t);
+	cell->setPort(ID(Y_t), sig_y_t);
+	cell->set_src_attribute(src);
+	return cell;
+}
+
+RTLIL::Cell * PIFTWorker::addTaintCell_2I1O(
+	RTLIL::Module *module, const std::string &type,
+	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_b, const RTLIL::SigSpec &sig_y,
+	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_b_t, const RTLIL::SigSpec &sig_y_t, 
+	bool is_signed, const std::string &src) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_2I1O));
+	cell->setParam(ID(TYPE), type);
+	cell->setParam(ID::A_SIGNED, is_signed);
+	cell->setParam(ID::B_SIGNED, is_signed);
+	cell->setParam(ID::A_WIDTH, sig_a.size());
+	cell->setParam(ID::B_WIDTH, sig_b.size());
+	cell->setParam(ID::Y_WIDTH, sig_y.size());
+	cell->setPort(ID::A, sig_a);
+	cell->setPort(ID::B, sig_b);
+	cell->setPort(ID::Y, sig_y);
+	cell->setPort(ID(A_t), sig_a_t);
+	cell->setPort(ID(B_t), sig_b_t);
+	cell->setPort(ID(Y_t), sig_y_t);
+	cell->set_src_attribute(src);
+	return cell;
+}
+
+RTLIL::Cell * PIFTWorker::addTaintCell_mux(
+	RTLIL::Module *module, const std::string &type,
+	const RTLIL::SigSpec &sig_a, const RTLIL::SigSpec &sig_b, const RTLIL::SigSpec &sig_s, const RTLIL::SigSpec &sig_y, 
+	const RTLIL::SigSpec &sig_a_t, const RTLIL::SigSpec &sig_b_t, const RTLIL::SigSpec &sig_s_t, const RTLIL::SigSpec &sig_y_t, 
+	const std::string &src) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_mux));
+	cell->setParam(ID(TYPE), type);
+	cell->setParam(ID::WIDTH, sig_a.size());
+	cell->setParam(ID::S_WIDTH, sig_s.size());
+	cell->setPort(ID::A, sig_a);
+	cell->setPort(ID::B, sig_b);
+	cell->setPort(ID::S, sig_s);
+	cell->setPort(ID::Y, sig_y);
+	cell->setPort(ID(A_t), sig_a_t);
+	cell->setPort(ID(B_t), sig_b_t);
+	cell->setPort(ID(S_t), sig_s_t);
+	cell->setPort(ID(Y_t), sig_y_t);
+	cell->set_src_attribute(src);
+	return cell;
+}
+
+RTLIL::Cell* PIFTWorker::addTaintCell_sdff(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_srst, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	RTLIL::Const srst_value, bool clk_polarity, bool srst_polarity, const std::string &src) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_sdff));
+	cell->setParam(ID::CLK_POLARITY, clk_polarity);
+	cell->setParam(ID::SRST_POLARITY, srst_polarity);
+	cell->setParam(ID::SRST_VALUE, srst_value);
+	cell->setParam(ID::WIDTH, sig_q.size());
+	cell->setPort(ID::CLK, sig_clk);
+	cell->setPort(ID::SRST, sig_srst);
+	cell->setPort(ID::D, sig_d);
+	cell->setPort(ID::Q, sig_q);
+	cell->setPort(ID(D_t), sig_d_t);
+	cell->setPort(ID(Q_t), sig_q_t);
+	cell->set_src_attribute(src);
+	cell->set_bool_attribute(ID(tainted), true);
+	return cell;
+}
+
+RTLIL::Cell* PIFTWorker::addTaintCell_sdffe(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_srst, const RTLIL::SigSpec &sig_en, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	RTLIL::Const srst_value, bool clk_polarity, bool en_polarity, bool srst_polarity, const std::string &src) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_sdffe));
+	cell->parameters[ID::CLK_POLARITY] = clk_polarity;
+	cell->parameters[ID::EN_POLARITY] = en_polarity;
+	cell->parameters[ID::SRST_POLARITY] = srst_polarity;
+	cell->parameters[ID::SRST_VALUE] = srst_value;
+	cell->parameters[ID::WIDTH] = sig_q.size();
+	cell->setPort(ID::CLK, sig_clk);
+	cell->setPort(ID::SRST, sig_srst);
+	cell->setPort(ID::EN, sig_en);
+	cell->setPort(ID::D, sig_d);
+	cell->setPort(ID::Q, sig_q);
+	cell->setPort(ID(EN_t), sig_en_t);
+	cell->setPort(ID(D_t), sig_d_t);
+	cell->setPort(ID(Q_t), sig_q_t);
+	cell->set_src_attribute(src);
+	cell->set_bool_attribute(ID(tainted), true);
+	return cell;
+}
+
+void PIFTWorker::addTaintCell_sdffce(
+	RTLIL::Module *module, RTLIL::Cell *origin,
+	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_sdffce));
+	cell->parameters = origin->parameters;
+	cell->setPort(ID::CLK, origin->getPort(ID::CLK));
+	cell->setPort(ID::SRST, origin->getPort(ID::SRST));
+	cell->setPort(ID::EN, origin->getPort(ID::EN));
+	cell->setPort(ID::D, origin->getPort(ID::D));
+	cell->setPort(ID::Q, origin->getPort(ID::Q));
+	cell->setPort(ID(EN_t), sig_en_t);
+	cell->setPort(ID(D_t), sig_d_t);
+	cell->setPort(ID(Q_t), sig_q_t);
+	cell->set_src_attribute(origin->get_src_attribute());
+	cell->set_bool_attribute(ID(tainted), true);
+}
+
+void PIFTWorker::addTaintCell_adff(
+	RTLIL::Module *module, RTLIL::Cell *origin, 
+	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_adff));
+	cell->parameters = origin->parameters;
+	cell->setPort(ID::CLK, origin->getPort(ID::CLK));
+	cell->setPort(ID::ARST, origin->getPort(ID::ARST));
+	cell->setPort(ID::D, origin->getPort(ID::D));
+	cell->setPort(ID::Q, origin->getPort(ID::Q));
+	cell->setPort(ID(D_t), sig_d_t);
+	cell->setPort(ID(Q_t), sig_q_t);
+	cell->set_src_attribute(origin->get_src_attribute());
+	cell->set_bool_attribute(ID(tainted), true);
+}
+
+RTLIL::Cell* PIFTWorker::addTaintCell_dff(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	bool clk_polarity, const std::string &src) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_dff));
+	cell->setParam(ID::CLK_POLARITY, clk_polarity);
+	cell->setParam(ID::WIDTH, sig_q.size());
+	cell->setPort(ID::CLK, sig_clk);
+	cell->setPort(ID::D, sig_d);
+	cell->setPort(ID::Q, sig_q);
+	cell->setPort(ID(D_t), sig_d_t);
+	cell->setPort(ID(Q_t), sig_q_t);
+	cell->set_src_attribute(src);
+	cell->set_bool_attribute(ID(tainted), true);
+	return cell;
+}
+
+RTLIL::Cell* PIFTWorker::addTaintCell_dffe(
+	RTLIL::Module *module, 
+	const RTLIL::SigSpec &sig_clk, const RTLIL::SigSpec &sig_en, const RTLIL::SigSpec &sig_d, const RTLIL::SigSpec &sig_q,
+	const RTLIL::SigSpec &sig_en_t, const RTLIL::SigSpec &sig_d_t, const RTLIL::SigSpec &sig_q_t,
+	bool clk_polarity, bool en_polarity, const std::string &src) {
+	RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_dffe));
+	cell->setParam(ID::CLK_POLARITY, clk_polarity);
+	cell->setParam(ID::EN_POLARITY, en_polarity);
+	cell->setParam(ID::WIDTH, sig_q.size());
+	cell->setPort(ID::CLK, sig_clk);
+	cell->setPort(ID::EN, sig_en);
+	cell->setPort(ID::D, sig_d);
+	cell->setPort(ID::Q, sig_q);
+	cell->setPort(ID(EN_t), sig_en_t);
+	cell->setPort(ID(D_t), sig_d_t);
+	cell->setPort(ID(Q_t), sig_q_t);
+	cell->set_src_attribute(src);
+	cell->set_bool_attribute(ID(tainted), true);
+	return cell;
+}
+
+void PIFTWorker::addTaintCell_mem(RTLIL::Module *module, RTLIL::Cell *origin) {
+	enum PORT_NAME {
+		RD_CLK, RD_EN, RD_ARST, RD_SRST, RD_ADDR, RD_DATA,
+		WR_CLK, WR_EN, WR_ADDR, WR_DATA,
+		PORT_NUM
+	};
+	RTLIL::SigSpec port[PORT_NUM] = {
+		get_port(origin, ID::RD_CLK),
+		get_port(origin, ID::RD_EN),
+		get_port(origin, ID::RD_ARST),
+		get_port(origin, ID::RD_SRST),
+		get_port(origin, ID::RD_ADDR),
+		get_port(origin, ID::RD_DATA),
+		get_port(origin, ID::WR_CLK),
+		get_port(origin, ID::WR_EN),
+		get_port(origin, ID::WR_ADDR),
+		get_port(origin, ID::WR_DATA)
+	};
+	std::vector<RTLIL::SigSpec> port_taint[PORT_NUM] = {
+		get_taint_signals(module, port[RD_CLK]),
+		get_taint_signals(module, port[RD_EN]),
+		get_taint_signals(module, port[RD_ARST]),
+		get_taint_signals(module, port[RD_SRST]),
+		get_taint_signals(module, port[RD_ADDR]),
+		get_taint_signals(module, port[RD_DATA]),
+		get_taint_signals(module, port[WR_CLK]),
+		get_taint_signals(module, port[WR_EN]),
+		get_taint_signals(module, port[WR_ADDR]),
+		get_taint_signals(module, port[WR_DATA])
+	};
+
+	for (unsigned long taint_id = 0; taint_id < taint_num; taint_id++) {
+		RTLIL::Cell *cell = module->addCell(NEW_ID, ID(taintcell_mem));
+		cell->parameters = origin->parameters;
+		cell->set_src_attribute(origin->get_src_attribute());
+		cell->set_bool_attribute(ID(tainted), true);
+
+		cell->setPort(ID::RD_CLK, port[RD_CLK]);
+		cell->setPort(ID::RD_EN, port[RD_EN]);
+		cell->setPort(ID::RD_ARST, port[RD_ARST]);
+		cell->setPort(ID::RD_SRST, port[RD_SRST]);
+		cell->setPort(ID::RD_ADDR, port[RD_ADDR]);
+		cell->setPort(ID::RD_DATA, port[RD_DATA]);
+		cell->setPort(ID::WR_CLK, port[WR_CLK]);
+		cell->setPort(ID::WR_EN, port[WR_EN]);
+		cell->setPort(ID::WR_ADDR, port[WR_ADDR]);
+		cell->setPort(ID::WR_DATA, port[WR_DATA]);
+		
+		cell->setPort(ID(RD_EN_t), port_taint[RD_EN][taint_id]);
+		cell->setPort(ID(RD_ADDR_t), port_taint[RD_ADDR][taint_id]);
+		cell->setPort(ID(RD_DATA_t), port_taint[RD_DATA][taint_id]);
+		cell->setPort(ID(WR_EN_t), port_taint[WR_EN][taint_id]);
+		cell->setPort(ID(WR_ADDR_t), port_taint[WR_ADDR][taint_id]);
+		cell->setPort(ID(WR_DATA_t), port_taint[WR_DATA][taint_id]);
+	}
+}
+
 
 struct ProgrammableIFTPass : public Pass {
 	ProgrammableIFTPass() : Pass("pift") {}
